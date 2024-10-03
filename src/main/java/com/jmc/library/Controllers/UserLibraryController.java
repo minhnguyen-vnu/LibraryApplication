1ca2695c85e5355e690b454dd38896972a2be7e7
lam-nt-825921 <23020096@vnu.edu.vn>
2024-10-03 16:08:34 +0700

    userView_1

---
+++ src/main/java/com/jmc/library/Controllers/UserLibraryController.java
@@ -0,0 +1,11 @@
+package com.jmc.library.Controllers;
+
+import javafx.scene.control.*;
+
+
+public class UserLibraryController {
+    public DatePicker clock;
+    private Button menu_menu_btn;
+    private TextField search_fld;
+    private Button search_btn;
+}
