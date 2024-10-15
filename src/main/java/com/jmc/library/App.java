202ef37dd376bb0ee4c70d9634238b54eb7916b3
lam-nt-825921 <23020096@vnu.edu.vn>
2024-10-15 10:59:20 +0700

    changes

--- src/main/java/com/jmc/library/App.java
+++ src/main/java/com/jmc/library/App.java
@@ -12,7 +12,7 @@ import java.sql.SQLOutput;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Model.getInstance().getViewFactory().showAuthenticationWindow();
+        Model.getInstance().getViewFactory().showUserLibrary();
     }
 
     public static void main(String[] args) {
