b2a338923c52901dda0fd5d8be99239f0b6cf5a0
lam-nt-825921 <23020096@vnu.edu.vn>
2024-10-17 21:54:20 +0700

    add back to dashboard button

--- src/main/java/com/jmc/library/Controllers/LibraryControllers/AdminLibraryController.java
+++ src/main/java/com/jmc/library/Controllers/LibraryControllers/AdminLibraryController.java
@@ -2,6 +2,7 @@ package com.jmc.library.Controllers.LibraryControllers;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
@@ -16,6 +17,9 @@ public class AdminLibraryController extends LibraryController implements Initial
     public Button go_to_setting_btn;
     public Button manage_btn;
     public Button go_to_library_btn;
+    public Button back_to_dashboard_btn;
+    public Button go_to_dashboard_btn;
+    public ChoiceBox num_row_shown;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
