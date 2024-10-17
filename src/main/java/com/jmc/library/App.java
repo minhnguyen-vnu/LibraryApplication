6add7305299345488aec6541a0eaa1333a76020d
lam-nt-825921 <23020096@vnu.edu.vn>
2024-10-17 22:07:47 +0700

    pre pull FrontEnd

--- src/main/java/com/jmc/library/App.java
+++ src/main/java/com/jmc/library/App.java
@@ -12,7 +12,7 @@ import java.sql.SQLOutput;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Model.getInstance().getViewFactory().showAdminLibrary();
+        Model.getInstance().getViewFactory().showAuthenticationWindow();
     }
 
     public static void main(String[] args) {
