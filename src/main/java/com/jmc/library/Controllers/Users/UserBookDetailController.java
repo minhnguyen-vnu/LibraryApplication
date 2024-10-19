90be5590ed3f6c6380774acb87c16626759fbee2
lam-nt-825921 <23020096@vnu.edu.vn>
2024-10-19 13:24:28 +0700

    link book detail to controller

---
+++ src/main/java/com/jmc/library/Controllers/Users/UserBookDetailController.java
@@ -0,0 +1,14 @@
+package com.jmc.library.Controllers.Users;
+
+import javafx.scene.control.Label;
+import javafx.scene.text.TextFlow;
+
+public class UserBookDetailController {
+    public TextFlow book_name_txt_flw;
+    public Label publication_date_lbl;
+    public Label award_lbl;
+    public Label nomination_lbl;
+    public Label character_lbl;
+    public Label genre_lbl;
+    public Label original_language_lbl;
+}
