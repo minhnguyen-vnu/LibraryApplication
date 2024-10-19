5e4922b5b2e7b720ac2276a40d7cf6387c1f80b8
lam-nt-825921 <23020096@vnu.edu.vn>
2024-10-19 13:53:29 +0700

    changes

--- src/main/java/com/jmc/library/Controllers/Users/UserBookDetailController.java
+++ src/main/java/com/jmc/library/Controllers/Users/UserBookDetailController.java
@@ -1,6 +1,8 @@
 package com.jmc.library.Controllers.Users;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.TextFlow;
 
 public class UserBookDetailController {
@@ -11,4 +13,7 @@ public class UserBookDetailController {
     public Label character_lbl;
     public Label genre_lbl;
     public Label original_language_lbl;
+    public ImageView book_img;
+    public Button get_book_btn;
+    public ImageView go_to_back_btn;
 }
