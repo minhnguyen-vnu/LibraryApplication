490ba9a17b8576ea6cf1a1e6b6f1656a2cf5f5c7
lam-nt-825921 <23020096@vnu.edu.vn>
2024-10-30 20:24:53 +0700

    cart

---
+++ src/main/java/com/jmc/library/Controllers/Users/CartEntityController.java
@@ -0,0 +1,16 @@
+package com.jmc.library.Controllers.Users;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class CartEntityController {
+    public ImageView book_img;
+    public Label book_name_lbl;
+    public Label author_lbl;
+    public Button decrease_btn;
+    public Label quantity_lbl;
+    public Button increase_btn;
+    public Label cost_lbl;
+    public Button erase_btn;
+}
---
+++ src/main/java/com/jmc/library/Controllers/Users/UserCartController.java
@@ -0,0 +1,17 @@
+package com.jmc.library.Controllers.Users;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class UserCartController {
+    public Button back_library_btn;
+    public ChoiceBox sort_choice_box;
+    public VBox list_book_vbox;
+    public Label sub_total_lbl;
+    public Label additional_lbl;
+    public Label total_lbl;
+    public Button check_out_btn;
+    public Label show_total_lbl;
+}
