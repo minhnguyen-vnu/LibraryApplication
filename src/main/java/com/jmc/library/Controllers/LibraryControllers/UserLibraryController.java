691f372c99c4d2e15c5ed3636bc051b2853113cd
NamDD1234 <160829136+NamDD1234@users.noreply.github.com>
2024-11-05 10:27:24 +0700

    prepull

--- src/main/java/com/jmc/library/Controllers/LibraryControllers/UserLibraryController.java
+++ src/main/java/com/jmc/library/Controllers/LibraryControllers/UserLibraryController.java
@@ -10,10 +10,12 @@ import com.mysql.cj.xdevapi.CreateIndexParams;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -34,14 +36,17 @@ public class UserLibraryController extends LibraryController implements Initiali
     public ImageView account_avatar_img;
     public TableColumn<BookInfo, Boolean> add_to_cart_tb_cl;
     public TableColumn<BookInfo, String> book_name_tb_cl;
-    public ChoiceBox num_row_shown;
+    public ChoiceBox<Integer> num_row_shown;
     public Button go_to_dashboard_btn;
     public Button go_to_store_btn;
+    public AnchorPane matte_screen;
+    public AnchorPane user_info_pane;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setButtonListener();
+        setMaterialListener();
         initialAction();
         addBinding();
         onAction();
@@ -57,6 +62,49 @@ public class UserLibraryController extends LibraryController implements Initiali
         });
         go_to_store_btn.setOnAction(actionEvent -> {
             Model.getInstance().getViewFactory().getSelectedUserMode().set("User Store");
+        });
+        log_out_btn.setOnAction(actionEvent -> {
+            Model.getInstance().getViewFactory().showAuthenticationWindow();
+            Stage currentStage = (Stage) log_out_btn.getScene().getWindow();
+            Model.getInstance().getViewFactory().closeStage(currentStage);
+        });
+    }
+
+    private void setMaterialListener() {
+
+        username_lbl.setText(LibraryModel.getInstance().getUser().getUsername());
+
+        num_row_shown.getItems().addAll(5, 10, 15, 20);
+        num_row_shown.setValue(5);
+        num_row_shown.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            store_tb.setItems(FXCollections.observableArrayList(bookList.stream().limit(newVal).collect(Collectors.toList())));
+        });
+
+        bookList.addListener((ListChangeListener<BookInfo>) change -> {
+            store_tb.setItems(FXCollections.observableArrayList(bookList.stream().limit(num_row_shown.getValue()).collect(Collectors.toList())));
+        });
+
+        account_avatar_img.setOnMouseClicked(mouseEvent -> {
+            if(user_info_pane.getChildren().isEmpty()) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/UserInfo.fxml"));
+                try {
+                    user_info_pane.getChildren().add(loader.load());
+                    matte_screen.setVisible(true);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                user_info_pane.getChildren().clear();
+                matte_screen.setVisible(false);
+            }
+        });
+        matte_screen.setOnMouseClicked(mouseEvent -> {
+            if(!user_info_pane.getChildren().isEmpty()) {
+                user_info_pane.getChildren().clear();
+                matte_screen.setVisible(false);
+            }
         });
     }
 
@@ -117,11 +165,11 @@ public class UserLibraryController extends LibraryController implements Initiali
                     setText(book.getBookName());
                     setOnMouseClicked(event -> {
                         BookModel.getInstance().setBookInfo(book);
-                        System.out.println("Book name: " + book.getBookName());
+                        //System.out.println("Book name: " + book.getBookName());
                         Stage currentStage = (Stage) go_to_user_library_btn.getScene().getWindow();
                         Model.getInstance().getViewFactory().closeStage(currentStage);
                         Model.getInstance().getViewFactory().showDisplayBook();
-                        System.out.println("Book name: " + book.getBookName());
+                        //System.out.println("Book name: " + book.getBookName());
                     });
                 }
             }
