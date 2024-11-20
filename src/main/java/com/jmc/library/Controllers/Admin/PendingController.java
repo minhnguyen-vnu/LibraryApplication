45e0e456f171e749b9798ca52d3daa96da9da826
lam-nt-825921 <23020096@vnu.edu.vn>
2024-11-20 12:51:35 +0700

    changes

--- src/main/java/com/jmc/library/Controllers/Admin/PendingController.java
+++ src/main/java/com/jmc/library/Controllers/Admin/PendingController.java
@@ -1,21 +1,23 @@
 package com.jmc.library.Controllers.Admin;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 
 public class PendingController {
 
-    /** need make assets IssueInfo or some things like BookInfo*/
-    public TableView/*<IssueInfo>*/ store_tb;
-    public TableColumn issue_id_tb_cl;
-    public TableColumn book_id_tb_cl;
-    public TableColumn customer_tb_cl;
-    public TableColumn borrow_date_tb_cl;
-    public TableColumn due_date_tb_cl;
-    public TableColumn cost_tb_cl;
-    /** This column is where select the status for issue */
-    public TableColumn/*<IssueInfo, ChoiceBox>*/ status_tb_cl;
-
+   
+    public TextField get_customer_name_txt_fld;
+    public DatePicker get_borrowed_date;
+    public DatePicker get_due_date;
+    public ChoiceBox status_choice_box;
+    public Button search_btn;
     public Button update_btn;
+    public Button return_btn;
+    public TableView store_tb;
+    public TableColumn issue_id_cl;
+    public TableColumn book_id_cl;
+    public TableColumn customer_cl;
+    public TableColumn borrowed_date_cl;
+    public TableColumn due_date_cl;
+    public TableColumn cost_cl;
+    public TableColumn status_cl;
 }
