f741230e37c6f3866f0724dfb053bb6889bcdf3b
Minh Nguyen <23020114@vnu.edu.vn>
2024-11-21 18:37:05 +0700

    changes

---
+++ src/main/java/com/jmc/library/Controllers/Display/BookDisplayController.java
@@ -0,0 +1,14 @@
+package com.jmc.library.Controllers.Display;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookDisplayController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
