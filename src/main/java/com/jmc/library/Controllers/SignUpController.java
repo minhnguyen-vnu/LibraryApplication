cd0be0e9a2347a4cefa86534a9cf65d945a58933
lam-nt-825921 <23020096@vnu.edu.vn>
2024-11-23 10:17:29 +0700

    fix sign up

--- src/main/java/com/jmc/library/Controllers/SignUpController.java
+++ src/main/java/com/jmc/library/Controllers/SignUpController.java
@@ -3,10 +3,12 @@ package com.jmc.library.Controllers;
 import com.jmc.library.Database.DBUpdate;
 import com.jmc.library.Database.DBQuery;
 import com.jmc.library.Models.Model;
+import javafx.animation.RotateTransition;
 import javafx.application.Platform;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
@@ -27,9 +29,14 @@ public class SignUpController implements Initializable {
     public Label error_lbl;
     public PasswordField confirm_su;
     public Label back_lbl;
+    public ImageView loading_img;
+    public RotateTransition rotateTransition;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {addListener();}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        addListener();
+        setRotateTransition();
+    }
 
     public void addListener(){
         sign_up_btn.setOnAction(actionEvent -> signUp());
@@ -40,9 +47,36 @@ public class SignUpController implements Initializable {
             confirm_su.clear();
             password_su.clear();
         });
+        username_su.setOnKeyPressed(event -> {
+            if (event.getCode() == javafx.scene.input.KeyCode.ENTER) {
+                password_su.requestFocus();
+            }
+        });
+        password_su.setOnKeyPressed(event -> {
+            if (event.getCode() == javafx.scene.input.KeyCode.ENTER) {
+                confirm_su.requestFocus();
+            }
+        });
+        confirm_su.setOnKeyPressed(event -> {
+            if (event.getCode() == javafx.scene.input.KeyCode.ENTER) {
+                signUp();
+            }
+        });
+
+    }
+
+    public void setRotateTransition(){
+        rotateTransition = new RotateTransition();
+        rotateTransition.setNode(loading_img);
+        rotateTransition.setByAngle(360);
+        rotateTransition.setCycleCount(1000);
+        rotateTransition.setAutoReverse(false);
+        rotateTransition.play();
     }
 
     public void signUp(){
+        loading_img.setVisible(true);
+        rotateTransition.play();
         if(!Objects.equals(password_su.getText(), confirm_su.getText())){
             error_lbl.setText("Your Confirmation Password didn't match the Password");
             error_lbl.setStyle("-fx-text-fill: red");
@@ -53,7 +87,7 @@ public class SignUpController implements Initializable {
                 ResultSet resultSet = dbQuery.getValue();
                 try {
                     if (!resultSet.next()) {
-                        DBUpdate dbUpdate = new DBUpdate("insert into users values(?, ?, ?, ?)", username_su.getText(), password_su.getText(), 0, LocalDate.now());
+                        DBUpdate dbUpdate = new DBUpdate("insert into users (username, password, isAdmin, registeredDate) values(?, ?, ?, ?)", username_su.getText(), password_su.getText(), 0, LocalDate.now());
                         dbUpdate.setOnSucceeded(event -> {
                             Platform.runLater(() -> {
                                 System.out.println("Update Successfully");
@@ -67,9 +101,13 @@ public class SignUpController implements Initializable {
                         Thread thread = new Thread(dbUpdate);
                         thread.setDaemon(true);
                         thread.start();
+                        loading_img.setVisible(false);
+                        rotateTransition.stop();
                         error_lbl.setText("Account is created succesfully!");
                         error_lbl.setStyle("-fx-text-fill: green");
                     } else {
+                        loading_img.setVisible(false);
+                        rotateTransition.stop();
                         error_lbl.setText("Account name already exists");
                         error_lbl.setStyle("-fx-text-fill: red;");
                         BorderPane.setAlignment(error_lbl, Pos.CENTER);
